7bd37a24cca933c04d759895040985e5ac635c2d
Lucile Jeanneret <lujeanne@microsoft.com>
2025-03-25 11:10:43 +0100

    add sql script

--- infra-tf/sql-databases.tf
+++ infra-tf/sql-databases.tf
@@ -40,7 +40,98 @@ resource "azurerm_mssql_firewall_rule" "identity_allow_all" {
   end_ip_address   = "255.255.255.254"
 }
 
-# Store database connection strings in Key Vault
+# Database user setup scripts
+resource "azurerm_resource_group_deployment_script" "catalog_db_setup" {
+  name                = "catalog-db-setup-script"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  
+  retention_interval = "PT1H"
+  cleanup_preference = "OnSuccess"
+
+  command            = <<-EOT
+    wget https://github.com/microsoft/go-sqlcmd/releases/download/v0.8.1/sqlcmd-v0.8.1-linux-x64.tar.bz2
+    tar x -f sqlcmd-v0.8.1-linux-x64.tar.bz2 -C .
+
+    cat <<SCRIPT_END > ./initDb.sql
+    IF EXISTS (SELECT * FROM sys.database_principals WHERE name = '${var.app_user_name}')
+    BEGIN
+        DROP USER [${var.app_user_name}]
+    END
+    GO
+    CREATE USER [${var.app_user_name}] WITH PASSWORD = '${var.app_user_password}'
+    GO
+    ALTER ROLE db_owner ADD MEMBER [${var.app_user_name}]
+    GO
+    SCRIPT_END
+
+    ./sqlcmd -S ${azurerm_mssql_server.catalog.fully_qualified_domain_name} -d ${azurerm_mssql_database.catalog.name} -U ${var.sql_admin_username} -P '${var.sql_admin_password}' -i ./initDb.sql
+  EOT
+
+  environment_variables = {
+    "APPUSERNAME"      = var.app_user_name
+    "DBNAME"           = azurerm_mssql_database.catalog.name
+    "DBSERVER"         = azurerm_mssql_server.catalog.fully_qualified_domain_name
+    "SQLADMIN"         = var.sql_admin_username
+  }
+
+  secure_environment_variables = {
+    "APPUSERPASSWORD" = var.app_user_password
+    "SQLCMDPASSWORD"  = var.sql_admin_password
+  }
+
+  depends_on = [
+    azurerm_mssql_database.catalog,
+    azurerm_mssql_server.catalog
+  ]
+}
+
+resource "azurerm_resource_group_deployment_script" "identity_db_setup" {
+  name                = "identity-db-setup-script"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  
+  retention_interval = "PT1H"
+  cleanup_preference = "OnSuccess"
+
+  command            = <<-EOT
+    wget https://github.com/microsoft/go-sqlcmd/releases/download/v0.8.1/sqlcmd-v0.8.1-linux-x64.tar.bz2
+    tar x -f sqlcmd-v0.8.1-linux-x64.tar.bz2 -C .
+
+    cat <<SCRIPT_END > ./initDb.sql
+    IF EXISTS (SELECT * FROM sys.database_principals WHERE name = '${var.app_user_name}')
+    BEGIN
+        DROP USER [${var.app_user_name}]
+    END
+    GO
+    CREATE USER [${var.app_user_name}] WITH PASSWORD = '${var.app_user_password}'
+    GO
+    ALTER ROLE db_owner ADD MEMBER [${var.app_user_name}]
+    GO
+    SCRIPT_END
+
+    ./sqlcmd -S ${azurerm_mssql_server.identity.fully_qualified_domain_name} -d ${azurerm_mssql_database.identity.name} -U ${var.sql_admin_username} -P '${var.sql_admin_password}' -i ./initDb.sql
+  EOT
+
+  environment_variables = {
+    "APPUSERNAME"      = var.app_user_name
+    "DBNAME"           = azurerm_mssql_database.identity.name
+    "DBSERVER"         = azurerm_mssql_server.identity.fully_qualified_domain_name
+    "SQLADMIN"         = var.sql_admin_username
+  }
+
+  secure_environment_variables = {
+    "APPUSERPASSWORD" = var.app_user_password
+    "SQLCMDPASSWORD"  = var.sql_admin_password
+  }
+
+  depends_on = [
+    azurerm_mssql_database.identity,
+    azurerm_mssql_server.identity
+  ]
+}
+
+# Store database secrets in Key Vault
 resource "azurerm_key_vault_secret" "catalog_admin_password" {
   name         = "CatalogSqlAdminPassword"
   value        = var.sql_admin_password
