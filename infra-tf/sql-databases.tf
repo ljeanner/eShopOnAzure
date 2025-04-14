2e830e4c3cd258d26232033553f6cf4789c09803
Lucile Jeanneret <lujeanne@microsoft.com>
2025-04-14 19:41:46 +0200

    updat scripts

--- infra-tf/sql-databases.tf
+++ infra-tf/sql-databases.tf
@@ -1,135 +1,78 @@
 # SQL Databases and related resources
- 
- # Catalog Database
- resource "azurerm_mssql_database" "catalog" {
-   name           = "db-catalog"
-   server_id      = azurerm_mssql_server.catalog.id
-   collation      = "SQL_Latin1_General_CP1_CI_AS"
-   license_type   = "LicenseIncluded"
-   max_size_gb    = 2
-   sku_name       = "Basic"
-   zone_redundant = false
-   min_capacity   = 0
- }
- 
- # Identity Database
- resource "azurerm_mssql_database" "identity" {
-   name           = "db-identity"
-   server_id      = azurerm_mssql_server.identity.id
-   collation      = "SQL_Latin1_General_CP1_CI_AS"
-   license_type   = "LicenseIncluded"
-   max_size_gb    = 2
-   sku_name       = "Basic"
-   zone_redundant = false
-   min_capacity   = 0
- }
- 
- # Firewall rules for Catalog database
- resource "azurerm_mssql_firewall_rule" "catalog_allow_all" {
-   name             = "AllowAll"
-   server_id        = azurerm_mssql_server.catalog.id
-   start_ip_address = "0.0.0.1"
-   end_ip_address   = "255.255.255.254"
- }
- 
- # Firewall rules for Identity database
- resource "azurerm_mssql_firewall_rule" "identity_allow_all" {
-   name             = "AllowAll"
-   server_id        = azurerm_mssql_server.identity.id
-   start_ip_address = "0.0.0.1"
-   end_ip_address   = "255.255.255.254"
- }
- 
- # Store database connection strings in Key Vault
- resource "azurerm_key_vault_secret" "catalog_admin_password" {
-   name         = "CatalogSqlAdminPassword"
-   value        = var.sql_admin_password
-   key_vault_id = azurerm_key_vault.kv.id
- }
- 
- resource "azurerm_key_vault_secret" "identity_admin_password" {
-   name         = "IdentitySqlAdminPassword"
-   value        = var.sql_admin_password
-   key_vault_id = azurerm_key_vault.kv.id
- }
- 
- resource "azurerm_key_vault_secret" "catalog_app_user_password" {
-   name         = "CatalogAppUserPassword"
-   value        = var.app_user_password
-   key_vault_id = azurerm_key_vault.kv.id
- }
- 
- resource "azurerm_key_vault_secret" "identity_app_user_password" {
-   name         = "IdentityAppUserPassword"
-   value        = var.app_user_password
-   key_vault_id = azurerm_key_vault.kv.id
- }
- 
- # Connection strings with app user credentials
- resource "azurerm_key_vault_secret" "catalog_connection_string" {
-   name         = "CatalogConnectionString"
-   value        = "Server=${azurerm_mssql_server.catalog.fully_qualified_domain_name};Database=${azurerm_mssql_database.catalog.name};User=${var.app_user_name};Password=${var.app_user_password}"
-   key_vault_id = azurerm_key_vault.kv.id
- }
- 
- resource "azurerm_key_vault_secret" "identity_connection_string" {
-   name         = "IdentityConnectionString"
-   value        = "Server=${azurerm_mssql_server.identity.fully_qualified_domain_name};Database=${azurerm_mssql_database.identity.name};User=${var.app_user_name};Password=${var.app_user_password}"
-   key_vault_id = azurerm_key_vault.kv.id
- }
- 
- # SQL Database user setup for Catalog Database
-resource "null_resource" "setup_catalog_db_user" {
-  depends_on = [azurerm_mssql_database.catalog]
 
-  provisioner "local-exec" {
-    command = <<-EOT
-      # Creating SQL script file
-      cat > setup_catalog_user.sql << EOF
-      IF EXISTS (SELECT * FROM sys.database_principals WHERE name = '${var.app_user_name}')
-      BEGIN
-          DROP USER [${var.app_user_name}]
-      END
-      GO
-      CREATE USER [${var.app_user_name}] WITH PASSWORD = '${var.app_user_password}'
-      GO
-      ALTER ROLE db_owner ADD MEMBER [${var.app_user_name}]
-      GO
-      EOF
-
-      # Execute the SQL script using az cli
-      az sql db execute -g ${azurerm_resource_group.rg.name} -s ${azurerm_mssql_server.catalog.name} -n ${azurerm_mssql_database.catalog.name} -f setup_catalog_user.sql -u ${var.sql_admin_username} -p ${var.sql_admin_password} --connect-timeout 30
-
-      # Remove the temporary SQL file
-      rm -f setup_catalog_user.sql
-    EOT
-  }
+# Catalog Database
+resource "azurerm_mssql_database" "catalog" {
+  name           = "sqldb-catalog"
+  server_id      = azurerm_mssql_server.catalog.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "Basic"
+  zone_redundant = false
+  min_capacity   = 0
 }
 
-# SQL Database user setup for Identity Database
-resource "null_resource" "setup_identity_db_user" {
-  depends_on = [azurerm_mssql_database.identity]
+# Identity Database
+resource "azurerm_mssql_database" "identity" {
+  name           = "sqldb-identity"
+  server_id      = azurerm_mssql_server.identity.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "Basic"
+  zone_redundant = false
+  min_capacity   = 0
+}
 
-  provisioner "local-exec" {
-    command = <<-EOT
-      # Creating SQL script file
-      cat > setup_identity_user.sql << EOF
-      IF EXISTS (SELECT * FROM sys.database_principals WHERE name = '${var.app_user_name}')
-      BEGIN
-          DROP USER [${var.app_user_name}]
-      END
-      GO
-      CREATE USER [${var.app_user_name}] WITH PASSWORD = '${var.app_user_password}'
-      GO
-      ALTER ROLE db_owner ADD MEMBER [${var.app_user_name}]
-      GO
-      EOF
+# Firewall rules for Catalog database
+resource "azurerm_mssql_firewall_rule" "catalog_allow_all" {
+  name             = "AllowAll"
+  server_id        = azurerm_mssql_server.catalog.id
+  start_ip_address = "0.0.0.1"
+  end_ip_address   = "255.255.255.254"
+}
 
-      # Execute the SQL script using az cli
-      az sql db execute -g ${azurerm_resource_group.rg.name} -s ${azurerm_mssql_server.identity.name} -n ${azurerm_mssql_database.identity.name} -f setup_identity_user.sql -u ${var.sql_admin_username} -p ${var.sql_admin_password} --connect-timeout 30
+# Firewall rules for Identity database
+resource "azurerm_mssql_firewall_rule" "identity_allow_all" {
+  name             = "AllowAll"
+  server_id        = azurerm_mssql_server.identity.id
+  start_ip_address = "0.0.0.1"
+  end_ip_address   = "255.255.255.254"
+}
 
-      # Remove the temporary SQL file
-      rm -f setup_identity_user.sql
-    EOT
-  }
+# Store database connection strings in Key Vault
+resource "azurerm_key_vault_secret" "catalog_admin_password" {
+  name         = "CatalogSqlAdminPassword"
+  value        = var.sql_admin_password
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "identity_admin_password" {
+  name         = "IdentitySqlAdminPassword"
+  value        = var.sql_admin_password
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "catalog_app_user_password" {
+  name         = "CatalogAppUserPassword"
+  value        = var.app_user_password
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "identity_app_user_password" {
+  name         = "IdentityAppUserPassword"
+  value        = var.app_user_password
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "catalog_connection_string" {
+  name         = "CatalogConnectionString"
+  value        = "Server=${azurerm_mssql_server.catalog.fully_qualified_domain_name};Database=${azurerm_mssql_database.catalog.name};User=${var.app_user_name};Password=${var.app_user_password}"
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "identity_connection_string" {
+  name         = "IdentityConnectionString"
+  value        = "Server=${azurerm_mssql_server.identity.fully_qualified_domain_name};Database=${azurerm_mssql_database.identity.name};User=${var.app_user_name};Password=${var.app_user_password}"
+  key_vault_id = azurerm_key_vault.kv.id
 }
--- infra-tf/sql-servers.tf
+++ infra-tf/sql-servers.tf
@@ -39,6 +39,108 @@ resource "azurerm_mssql_server" "identity" {
   }
 }
 
-# Note: Catalog Firewall Rule is defined in sql-databases.tf
+# Deployment Script for User Management
+resource "azurerm_resource_group" "deployment_script_rg" {
+  name     = "deployment-script-rg"
+  location = azurerm_resource_group.rg.location
+}
 
-# Note: Database, firewall rules, and connection strings are defined in sql-databases.tf
+resource "azurerm_storage_account" "deployment_script_sa" {
+  name                     = "deploymentscript${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_container_registry" "deployment_script_acr" {
+  name                = "deploymentscriptacr${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+}
+
+# Replacing unsupported azurerm_deployment_script_azure_cli with azurerm_virtual_machine_extension
+resource "azurerm_virtual_machine_extension" "sql_user_setup" {
+  name                 = "sql-user-setup"
+  virtual_machine_id   = azurerm_virtual_machine.vm.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "az sql user create --name ${var.app_user_name} --password ${var.app_user_password} --database ${azurerm_mssql_database.catalog.name} --server ${azurerm_mssql_server.catalog.fully_qualified_domain_name} --admin-user ${var.sql_admin_username} --admin-password ${var.sql_admin_password}"
+    }
+SETTINGS
+
+  tags = var.tags
+}
+
+# Define the azurerm_virtual_machine resource
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vm${random_string.suffix.result}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
+  vm_size               = "Standard_B1s"
+
+  storage_os_disk {
+    name              = "osdisk${random_string.suffix.result}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = var.tags
+}
+
+# Define the azurerm_network_interface resource
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "nic-${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = var.tags
+}
+
+# Define the azurerm_subnet resource
+resource "azurerm_subnet" "vm_subnet" {
+  name                 = "snet-${random_string.suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vm_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Define the azurerm_virtual_network resource
+resource "azurerm_virtual_network" "vm_vnet" {
+  name                = "vnet-${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = var.tags
+}
